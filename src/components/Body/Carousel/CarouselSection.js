07bf87216ac6972f43c544eba2f773b07b2362f4
CCS-2022 <128265192+CCS-2022@users.noreply.github.com>
2023-04-01 14:08:41 -0400

    Updating with new pictures

--- src/components/Body/Carousel/CarouselSection.js
+++ src/components/Body/Carousel/CarouselSection.js
@@ -1,45 +1,172 @@
 import Carousel from "react-bootstrap/Carousel";
-import galleryOne from "../../../assets/gallery-1_resized.jpg";
-import galleryTwo from "../../../assets/gallery-2_resized.jpg";
-import galleryThree from "../../../assets/gallery-3_resized.jpg";
 import "./CarouselSection.css";
+import gallery1 from "../../../assets/gallery-19.jpeg";
+import gallery2 from "../../../assets/gallery-20.jpeg";
+import gallery3 from "../../../assets/gallery-21.jpeg";
+import gallery4 from "../../../assets/gallery-23.jpeg";
+import gallery5 from "../../../assets/gallery-13.jpeg";
+import gallery6 from "../../../assets/gallery-27.jpeg";
+import gallery7 from "../../../assets/gallery-41.jpeg";
+import gallery8 from "../../../assets/gallery-40.jpeg";
+import gallery9 from "../../../assets/gallery-42.jpeg";
+import gallery10 from "../../../assets/gallery-43.jpeg";
+import gallery11 from "../../../assets/gallery-45.jpeg";
+import gallery12 from "../../../assets/gallery-3.jpeg";
+import gallery13 from "../../../assets/gallery-9.jpeg";
+import gallery14 from "../../../assets/gallery-10.jpeg";
+import gallery15 from "../../../assets/gallery-11.jpeg";
+import gallery16 from "../../../assets/gallery-12.jpeg";
+import gallery17 from "../../../assets/gallery-14.jpeg";
+import gallery18 from "../../../assets/gallery-15.jpeg";
+import gallery19 from "../../../assets/gallery-16.jpeg";
+import gallery20 from "../../../assets/gallery-28.jpeg";
+import gallery21 from "../../../assets/gallery-31.jpeg";
+import gallery22 from "../../../assets/gallery-32.jpeg";
+import gallery23 from "../../../assets/gallery-35.jpeg";
+import gallery24 from "../../../assets/gallery-36.jpeg";
+import gallery25 from "../../../assets/gallery-39.jpeg";
+/*import gallery26 from "../../../assets/gallery-19.jpeg";
+import gallery27 from "../../../assets/gallery-20.jpeg";
+import gallery28 from "../../../assets/gallery-21.jpeg";
+import gallery29 from "../../../assets/gallery-13.jpeg";
+import gallery30 from "../../../assets/gallery-13.jpeg";
+import gallery31 from "../../../assets/gallery-19.jpeg";
+import gallery32 from "../../../assets/gallery-20.jpeg";
+import gallery33 from "../../../assets/gallery-21.jpeg";
+import gallery34 from "../../../assets/gallery-13.jpeg";
+import gallery35 from "../../../assets/gallery-13.jpeg";
+import gallery36 from "../../../assets/gallery-19.jpeg";
+import gallery37 from "../../../assets/gallery-20.jpeg";
+import gallery38 from "../../../assets/gallery-21.jpeg";
+import gallery39 from "../../../assets/gallery-13.jpeg";
+import gallery40 from "../../../assets/gallery-13.jpeg";*/
 
 function CarouselSection() {
   return (
     <div className="main-div" id="gallery">
       <p className="gallery-title">Previous Work</p>
       <Carousel className="carousel-view">
-        <Carousel.Item interval={1000}>
-          <img className="d-block carousel-img" src={galleryOne} alt="First slide" />
-          {/* <Carousel.Caption>
-            <h3>First slide label</h3>
-            <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
-          </Carousel.Caption> */}
-        </Carousel.Item>
-        <Carousel.Item interval={500}>
-          <img
-            className="d-block carousel-img"
-            src={galleryTwo}
-            alt="Second slide"
-          />
-          {/* <Carousel.Caption>
-            <h3>Second slide label</h3>
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-          </Carousel.Caption> */}
+        <Carousel.Item>
+          <img className="d-block carousel-img" src={gallery1 } alt="Not Found"/>
         </Carousel.Item>
         <Carousel.Item>
-          <img
-            className="d-block carousel-img"
-            src={galleryThree}
-            alt="Third slide"
-          />
-          {/* <Carousel.Caption>
-            <h3>Third slide label</h3>
-            <p>
-              Praesent commodo cursus magna, vel scelerisque nisl consectetur.
-            </p>
-          </Carousel.Caption> */}
+          <img className="d-block carousel-img" src={gallery2 } alt="Not Found"/>
         </Carousel.Item>
+        <Carousel.Item>
+          <img className="d-block carousel-img" src={gallery3 } alt="Not Found"/>
+        </Carousel.Item>
+        <Carousel.Item>
+          <img className="d-block carousel-img" src={gallery4 } alt="Not Found"/>
+        </Carousel.Item>
+        <Carousel.Item>
+          <img className="d-block carousel-img" src={gallery5 } alt="Not Found"/>
+        </Carousel.Item>
+        <Carousel.Item>
+          <img className="d-block carousel-img" src={gallery6 } alt="Not Found"/>
+        </Carousel.Item>
+        <Carousel.Item>
+          <img className="d-block carousel-img" src={gallery7 } alt="Not Found"/>
+        </Carousel.Item>
+        <Carousel.Item>
+          <img className="d-block carousel-img" src={gallery8 } alt="Not Found"/>
+        </Carousel.Item>
+        <Carousel.Item>
+          <img className="d-block carousel-img" src={gallery9 } alt="Not Found"/>
+        </Carousel.Item>
+        <Carousel.Item>
+          <img className="d-block carousel-img" src={gallery10} alt="Not Found"/>
+        </Carousel.Item>
+        <Carousel.Item>
+          <img className="d-block carousel-img" src={gallery11} alt="Not Found"/>
+        </Carousel.Item>
+        <Carousel.Item>
+          <img className="d-block carousel-img" src={gallery12} alt="Not Found"/>
+        </Carousel.Item>
+        <Carousel.Item>
+          <img className="d-block carousel-img" src={gallery13} alt="Not Found"/>
+        </Carousel.Item>
+        <Carousel.Item>
+          <img className="d-block carousel-img" src={gallery14} alt="Not Found"/>
+        </Carousel.Item>
+        <Carousel.Item>
+          <img className="d-block carousel-img" src={gallery15} alt="Not Found"/>
+        </Carousel.Item>
+        <Carousel.Item>
+          <img className="d-block carousel-img" src={gallery16} alt="Not Found"/>
+        </Carousel.Item>
+        <Carousel.Item>
+          <img className="d-block carousel-img" src={gallery17} alt="Not Found"/>
+        </Carousel.Item>
+        <Carousel.Item>
+          <img className="d-block carousel-img" src={gallery18} alt="Not Found"/>
+        </Carousel.Item>
+        <Carousel.Item>
+          <img className="d-block carousel-img" src={gallery19} alt="Not Found"/>
+        </Carousel.Item>
+        <Carousel.Item>
+          <img className="d-block carousel-img" src={gallery20} alt="Not Found"/>
+        </Carousel.Item>
+        <Carousel.Item>
+          <img className="d-block carousel-img" src={gallery21} alt="Not Found"/>
+        </Carousel.Item>
+        <Carousel.Item>
+          <img className="d-block carousel-img" src={gallery22} alt="Not Found"/>
+        </Carousel.Item>
+        <Carousel.Item>
+          <img className="d-block carousel-img" src={gallery23} alt="Not Found"/>
+        </Carousel.Item>
+        <Carousel.Item>
+          <img className="d-block carousel-img" src={gallery24} alt="Not Found"/>
+        </Carousel.Item>
+        <Carousel.Item>
+          <img className="d-block carousel-img" src={gallery25} alt="Not Found"/>
+        </Carousel.Item>
+
+{/*        <Carousel.Item>
+          <img className="d-block carousel-img" src={gallery26} alt="Not Found"/>
+        </Carousel.Item>
+        <Carousel.Item>
+          <img className="d-block carousel-img" src={gallery27} alt="Not Found"/>
+        </Carousel.Item>
+        <Carousel.Item>
+          <img className="d-block carousel-img" src={gallery28} alt="Not Found"/>
+        </Carousel.Item>
+        <Carousel.Item>
+          <img className="d-block carousel-img" src={gallery29} alt="Not Found"/>
+        </Carousel.Item>
+        <Carousel.Item>
+          <img className="d-block carousel-img" src={gallery30} alt="Not Found"/>
+        </Carousel.Item>
+        <Carousel.Item>
+          <img className="d-block carousel-img" src={gallery31} alt="Not Found"/>
+        </Carousel.Item>
+        <Carousel.Item>
+          <img className="d-block carousel-img" src={gallery32} alt="Not Found"/>
+        </Carousel.Item>
+        <Carousel.Item>
+          <img className="d-block carousel-img" src={gallery33} alt="Not Found"/>
+        </Carousel.Item>
+        <Carousel.Item>
+          <img className="d-block carousel-img" src={gallery34} alt="Not Found"/>
+        </Carousel.Item>
+        <Carousel.Item>
+          <img className="d-block carousel-img" src={gallery35} alt="Not Found"/>
+        </Carousel.Item>
+        <Carousel.Item>
+          <img className="d-block carousel-img" src={gallery36} alt="Not Found"/>
+        </Carousel.Item>
+        <Carousel.Item>
+          <img className="d-block carousel-img" src={gallery37} alt="Not Found"/>
+        </Carousel.Item>
+        <Carousel.Item>
+          <img className="d-block carousel-img" src={gallery38} alt="Not Found"/>
+        </Carousel.Item>
+        <Carousel.Item>
+          <img className="d-block carousel-img" src={gallery39} alt="Not Found"/>
+        </Carousel.Item>
+        <Carousel.Item>
+          <img className="d-block carousel-img" src={gallery40} alt="Not Found"/>
+        </Carousel.Item>*/}
       </Carousel>
     </div>
   );
